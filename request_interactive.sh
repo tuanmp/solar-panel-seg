a096c147ef97279624326e04ea7d1b6884149edd
pmtuan <tuanpham1503@gmail.com>
2026-06-28 01:13:04 -0700

    chore: remove stray files

--- request_interactive.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# gpu_requirement="cpu"
-gpu_requirement="gpu&hbm80g"
-salloc -A m2616_g -C $gpu_requirement -q shared_interactive --nodes 1 --ntasks-per-node 1 --gpus-per-task 1 --cpus-per-task 32 --time 01:00:00 --signal=SIGUSR1@180 #--image=tuanpham1503/torch_conda:0.4
